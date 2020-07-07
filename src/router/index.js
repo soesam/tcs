95b149d32ab92066c1a315bc7a69f9159abfd6e5
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-07 13:41:59 +0000

    updated router to include auth flow

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Test from "../views/Test.vue";
+import 'firebase/auth';
+import Test from "@/views/Test.vue";
+import Signin from '@/views/Signin.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
+    path: '*',
+    redirect: '/signin'
+  },
+  {
+    path: '/',
+    redirect: '/signin'
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  },
+  {
+    path: "/test",
     name: "Test",
     component: Test
   }
@@ -16,6 +31,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const user = firebase.auth().currentUser;
+  const shouldAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (shouldAuth && !user) next('signin');
+  else if (!shouldAuth && user) next('home');
+  else next();
 });
 
 export default router;
