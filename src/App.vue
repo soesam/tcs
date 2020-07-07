acd6d7527f88c887f83db6d96971b9e68e77fa6e
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-07 13:16:36 +0000

    placed firebase setup in main.js

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,3 @@
     <router-view />
   </div>
 </template>
-
-<script>
-import * as firebase from 'firebase'
-
-export default {
-  created() {
-
-  }
-}
-</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
+import * as firebase from "firebase/app";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import config from './firebase.config.js'
 
+firebase.initializeApp(config);
 Vue.config.productionTip = false;
 
 new Vue({
