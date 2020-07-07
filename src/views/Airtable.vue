ada8b91704e4b7271dde53e634eb8c070854da1e
Soe Sam <soesam65@gmail.com>
2020-07-07 16:18:49 +0100

    Atte,pt to add airtable

---
+++ src/views/Airtable.vue
@@ -0,0 +1,51 @@
+<template>
+  <table class="table table-bordered table-striped">
+    <thead>
+      <tr>
+        <th v-for="(col, colIndex) in columns" :key="colIndex">
+          {{ col }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="record in records" :key="record.id">
+        <td v-for="(col, colIndex) in columns" :key="record.id + '-' + colIndex">
+          {{ record.fields[col] }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'VueAirtable',
+  props: [
+    'columns'
+  ],
+  data: function () {
+    return {
+      apiUrl: 'https://api.airtable.com/v0/',
+      apiKey: 'keyLVnvjV4bFHXOaD', // Always use a read-only account token
+      base: 'Students',
+      records: []
+    };
+  },
+  mounted: function () {
+    this.getData();
+  },
+  methods: {
+    getData: function () {
+      axios({
+        url: this.apiUrl + this.base,
+        headers: {
+          'Authorization': `Bearer ${this.apiKey}`
+        }
+      }).then((res) => {
+        this.records = res.data.records;
+      });
+    }
+  }
+}
+</script>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,62 +1,20 @@
 <template>
   <div class="test">
     <h1>test page</h1>
+    <vue-airtable :columns="['Name', 'Sex', 'Points']"></vue-airtable>
     <Auth />
   </div>
-  <div class="Students">
-    <ul class="name"></ul>
-    <ul class="sname"></ul>
-    <ul class="email"></ul>
-    <ul class="gpoints"></ul>
-    <ul class="id"></ul>
-</div>
 </template>
 
 <script>
-const Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyLVnvjV4bFHXOaD'}).base('appvLWxrF80mDK8Xq');
+import Auth from "@/components/Auth.vue";
+import VueAirtable from './Airtable.vue'
 
 export default {
   name: "Test",
   components: {
-    Auth
-  },
-    methods: {
-      displayData() {
-          base('Student').select({
-              maxRecords: 3,
-              view: "Grid view"
-          }).eachPage(function page(records, fetchNextPage) {
-              let studentsname = document.querySelector(".Students ul.name")
-              let studentssname = document.querySelector(".Students ul.sname")
-              let studentsemail = document.querySelector(".Students ul.email")
-              let studentsgpoints = document.querySelector(".Students ul.gpoints")
-              let studentsid = document.querySelector(".Students ul.id")
-              studentsname.innerHTML = `<li></li>`
-              studentssname.innerHTML = `<li></li>`
-              studentsemail.innerHTML = `<li></li>`
-              studentsgpoints.innerHTML = `<li></li>`
-              studentsid.innerHTML = `<li></li>`
-              records.forEach(function(record) {
-                  console.log("Refresh")
-                  studentsname.innerHTML += `<li>${record.get('Name')}</li>`
-                  studentssname.innerHTML += `<li>${record.get('Last name')}</li>`
-                  studentsemail.innerHTML += `<li>${record.get('Email')}</li>`
-                  studentsgpoints.innerHTML += `<li>${record.get('Points')}</li>`
-                  studentsid.innerHTML += `<li>
-                                            <form onsubmit=pointUp("${record.id}") class="id">
-                                               <input type="number" class="${record.id}">
-                                               <input type="submit" value="Add">
-                                             </form>
-                                           </li>`
-              });
-
-              fetchNextPage();
-
-          }, function done(err) {
-              if (err) { console.error(err); return; }
-          });
-      }
-    }
+    Auth,
+    Airtable
   }
+};
 </script>
