56457d83dbc6f3f5b41926e97b20bc993979f5c8
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-07 11:30:04 +0000

    installed airtable

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,8 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
-
-
-Vue.use(VueRouter)
-
-
+Vue.use(VueRouter);
 
 const routes = [
   {
@@ -14,16 +10,12 @@ const routes = [
     name: "Home",
     component: Home
   }
-]
-
-
+];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
-})
-
-
+});
 
 export default router;
