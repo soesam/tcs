b8dc478feeea42935a4c0ecbb3c331717e01c6e2
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-07 11:18:40 +0000

    removed default content

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from "vue"
+import App from "./App.vue"
+import "./registerServiceWorker"
+import router from "./router"
 
-Vue.config.productionTip = false;
+
+
+Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount("#app")
