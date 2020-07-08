2c2dced1545cff9104669b895a204ffbf1b1ec23
Soe Sam <soesam65@gmail.com>
2020-07-08 16:40:52 +0100

    Added Green poinr buttons

--- src/views/Airtable.vue
+++ src/views/Airtable.vue
@@ -15,73 +15,74 @@
         >
           {{ record.fields[col] }}
         </td>
-        <v-btn color="green" text @click="pointUp(record.id)">+1</v-btn>
+        <button color="green" v-on:click="() => pointUp(record.id,1)">+1</button>
+        <button color="green" v-on:click="() => pointUp(record.id,5)">+5</button>
       </tr>
     </tbody>
   </table>
 </template>
 
 <script>
-  import axios from "axios";
-  var Airtable = require("airtable");
-  const sleep = function(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  export default {
-    name: "VueAirtable",
-    props: ["columns"],
-    data: function() {
-      return {
-        apiUrl: "https://api.airtable.com/v0/appvLWxrF80mDK8Xq/",
-        apiKey: "keyLVnvjV4bFHXOaD", // Always use a read-only account token
-        base: "Student",
-        records: []
-      };
-    },
-    mounted: function() {
-      this.getData();
-    },
-    methods: {
-
-      getData: function() {
-        axios({
-          url: this.apiUrl + this.base,
-          headers: {
-            Authorization: `Bearer ${this.apiKey}`
-          }
-        }).then(res => {
-          this.records = res.data.records;
-        });
-      },
-      pointUp: async function(key) {
-        var base = new Airtable({ apiKey: "keyLVnvjV4bFHXOaD" }).base(
-          "appvLWxrF80mDK8Xq"
-        );
-        var points = 0;
-        function fetch(err, record) {
-          if (err) {
-            console.error(err);
-            return;
-          }
-          points = record.get("Points");
-          let inputElement = document.querySelector(`input.${key}`);
-          let addition = parseInt(inputElement.value);
-          var newPoints = points + addition;
-          console.log(points);
-          console.log(addition);
-          console.log(newPoints);
-          base("Student").update([
-            {
-              id: key,
-              fields: {
-                Points: newPoints
-              }
-            }
-          ]);
+import axios from "axios";
+var Airtable = require("airtable");
+const sleep = function(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+export default {
+  name: "VueAirtable",
+  props: ["columns"],
+  data: function() {
+    return {
+      apiUrl: "https://api.airtable.com/v0/appvLWxrF80mDK8Xq/",
+      apiKey: "keyLVnvjV4bFHXOaD", // Always use a read-only account token
+      base: "Student",
+      records: []
+    };
+  },
+  mounted: function() {
+    this.getData();
+  },
+  methods: {
+    getData: function() {
+      axios({
+        url: this.apiUrl + this.base,
+        headers: {
+          Authorization: `Bearer ${this.apiKey}`
         }
-        base("Student").find(key, fetch);
-        await sleep(500);
+      }).then(res => {
+        this.records = res.data.records;
+      });
+    },
+    pointUp: async function(key, no) {
+      console.log("1");
+      console.log(key);
+      var base = new Airtable({ apiKey: "keyLVnvjV4bFHXOaD" }).base(
+        "appvLWxrF80mDK8Xq"
+      );
+      var points = 0;
+      function fetch(err, record) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        points = record.get("Points");
+        var newPoints = points + no;
+        console.log(points);
+        console.log(newPoints);
+        base("Student").update([
+          {
+            id: key,
+            fields: {
+              Points: newPoints
+            }
+          }
+        ]);
       }
+      base("Student").find(key, fetch);
+      await sleep(500);
+
+      this.getData();
     }
-  };
+  }
+};
 </script>
