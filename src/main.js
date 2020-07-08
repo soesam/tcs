75301b801b58b931eb2d7a365fd2fae4811acdbb
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 11:07:37 +0000

    removed production tip thing

--- src/main.js
+++ src/main.js
@@ -1,14 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import * as firebase from 'firebase/app'
-import 'firebase/auth'
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import 'firebase/auth';
+import 'firebase/firestore';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false;
-
-firebase.initializeApp({
+const config = {
   apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
   authDomain: "tcs-io.firebaseapp.com",
   databaseURL: "https://tcs-io.firebaseio.com",
@@ -16,7 +15,9 @@ firebase.initializeApp({
   storageBucket: "tcs-io.appspot.com",
   messagingSenderId: "302656240503",
   appId: "1:302656240503:web:eef537c72cf453cb92bf72"
-});
+};
+
+firebase.initializeApp(config);
 
 new Vue({
   router,
