14a44d0efb20ba99d10742ceb3a62ace023f0e1e
Soe Sam <soesam65@gmail.com>
2020-07-08 12:21:05 +0100

    Fixing error

--- src/views/Airtable.vue
+++ src/views/Airtable.vue
@@ -23,14 +23,13 @@
 
 <script>
 import axios from "axios";
-var Airtable = require('airtable');
+var Airtable = require("airtable");
 export default {
   name: "VueAirtable",
   props: ["columns"],
   data: function() {
     return {
-      apiUrl:
-        'https://api.airtable.com/v0/appvLWxrF80mDK8Xq/',
+      apiUrl: "https://api.airtable.com/v0/appvLWxrF80mDK8Xq/",
       apiKey: "keyLVnvjV4bFHXOaD", // Always use a read-only account token
       base: "Student",
       records: []
@@ -41,9 +40,9 @@ export default {
   },
   methods: {
     sleep: function(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
+      return new Promise(resolve => setTimeout(resolve, ms));
     },
-    
+
     getData: function() {
       axios({
         url: this.apiUrl + this.base,
@@ -55,29 +54,34 @@ export default {
       });
     },
     pointUp: async function(key) {
-        var base = new Airtable({apiKey: 'keyLVnvjV4bFHXOaD'}).base('appvLWxrF80mDK8Xq');
-        var points = 0;
-        function fetch(err, record) {
-          if (err) { console.error(err); return; }
-          points = record.get('Points');
-          let inputElement = document.querySelector(`input.${key}`)
-          let addition = parseInt(inputElement.value)
-          var newPoints = points + addition;
-          console.log(points)
-          console.log(addition)
-          console.log(newPoints)
-          base('Student').update([
-          {
-            "id": key,
-            "fields": {
-              "Points": newPoints,
-            }
-          },
-          ])
+      var base = new Airtable({ apiKey: "keyLVnvjV4bFHXOaD" }).base(
+        "appvLWxrF80mDK8Xq"
+      );
+      var points = 0;
+      function fetch(err, record) {
+        if (err) {
+          console.error(err);
+          return;
         }
-        base('Student').find( key , fetch)
-        await sleep(500)
-      },
-    },
+        points = record.get("Points");
+        let inputElement = document.querySelector(`input.${key}`);
+        let addition = parseInt(inputElement.value);
+        var newPoints = points + addition;
+        console.log(points);
+        console.log(addition);
+        console.log(newPoints);
+        base("Student").update([
+          {
+            id: key,
+            fields: {
+              Points: newPoints
+            }
+          }
+        ]);
+      }
+      base("Student").find(key, fetch);
+      await this.sleep(500);
+    }
   }
+};
 </script>
