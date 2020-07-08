741fb1ac82fffa285592d9a6ef96ef2dcc62ec4c
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 11:38:47 +0000

    centralized firebase stuff in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,8 @@
-export default {
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const app = firebase.initializeApp({
   apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
   authDomain: "tcs-io.firebaseapp.com",
   databaseURL: "https://tcs-io.firebaseio.com",
@@ -6,4 +10,7 @@ export default {
   storageBucket: "tcs-io.appspot.com",
   messagingSenderId: "302656240503",
   appId: "1:302656240503:web:eef537c72cf453cb92bf72"
-};
+});
+
+export const db = app.firestore();
+export const auth = app.auth();
--- src/firestore.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import config from './config'
-
-export const firestore = firebase.initializeApp({projectId: config.projectId})
-.firestore()
