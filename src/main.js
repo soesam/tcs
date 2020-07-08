75f7b0c63f523a52d434c76972fff9826d1845bc
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 11:02:51 +0000

    placed firebase initialize code in src/main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
+  authDomain: "tcs-io.firebaseapp.com",
+  databaseURL: "https://tcs-io.firebaseio.com",
+  projectId: "tcs-io",
+  storageBucket: "tcs-io.appspot.com",
+  messagingSenderId: "302656240503",
+  appId: "1:302656240503:web:eef537c72cf453cb92bf72"
+});
 
 new Vue({
   router,
