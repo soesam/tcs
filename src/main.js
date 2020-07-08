e06969bca93fb5030f00c5d545e9a21b4e8b3ffe
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 13:29:01 +0000

    added vuetify

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import "./worker";
 import router from "./router";
 import store from "./store";
 import { firestorePlugin } from "vuefire";
+import vuetify from "./plugins/vuetify";
 
 Vue.use(firestorePlugin);
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
