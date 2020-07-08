240309defdbf2ced8d4302b127203e76ae8a9d76
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 15:46:23 +0000

    started crating a authenticaiton test

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,21 @@
 <template>
-  <v-btn color="blue">hello</v-btn>
+  <v-btn large secondary light tile :click='authTest'>Sign in with Google</v-btn>
 </template>
+
+<script>
+import { db, auth } from '@/firebase'
+const ref = db.ref('')
+
+export default {
+  data: {
+    
+  }
+
+  methods: {
+    authTest() {
+      // trigger auth flow.
+     
+    } 
+  }
+}
+</script>
