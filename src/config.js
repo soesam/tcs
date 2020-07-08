71e40243da99cce8d99aa58368c49fb728c2ef5f
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-08 11:23:59 +0000

    installed vuefire

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
+  authDomain: "tcs-io.firebaseapp.com",
+  databaseURL: "https://tcs-io.firebaseio.com",
+  projectId: "tcs-io",
+  storageBucket: "tcs-io.appspot.com",
+  messagingSenderId: "302656240503",
+  appId: "1:302656240503:web:eef537c72cf453cb92bf72"
+};
--- src/main.js
+++ src/main.js
@@ -1,23 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import * as firebase from 'firebase/app'
-import 'firebase/auth';
-import 'firebase/firestore';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import { firestorePlugin } from 'vuefire';
 
-const config = {
-  apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
-  authDomain: "tcs-io.firebaseapp.com",
-  databaseURL: "https://tcs-io.firebaseio.com",
-  projectId: "tcs-io",
-  storageBucket: "tcs-io.appspot.com",
-  messagingSenderId: "302656240503",
-  appId: "1:302656240503:web:eef537c72cf453cb92bf72"
-};
-
-firebase.initializeApp(config);
+Vue.use(firestorePlugin);
 
 new Vue({
   router,
