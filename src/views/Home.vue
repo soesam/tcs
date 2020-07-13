bdef9639fcd32cfa5eea03d19b4e6d5a5cd10971
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-13 09:14:39 +0000

    made template home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='home'>
     <h1 class='display-4'>Welcome</h1>
-    <h2 class='display-3'>Welcome</h1>
+   <h2 class='display-3'>Welcome</h1>
     <h3 class='display-2'>Welcome</h1>
     <h4 class='display-1'>Welcome</h1>
   </div>
