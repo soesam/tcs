b583800152112ab060b61440090092ef4e4ec030
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-13 12:11:38 +0000

    started Signup component

---
+++ src/components/Signup.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name='email'
+                      type='email' 
+                      id='email'
+                      label='Email' 
+                      v-model='email'
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+ 
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name='password'
+                      type='password' 
+                      id='password' 
+                      label='Password' 
+                      v-model='password'
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type='submit'>Sign up</v-btn>
+                  </v-flex>
+                </v-layout>
+
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
