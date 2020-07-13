f15cd642e09c677cc3a97dcd6d0346ea7aca5d18
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-13 10:02:37 +0000

    fixing toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
-    <Header/>
-    <v-content>
-      <router-view />
-    </v-content>
-    <v-footer app></v-footer>
+    <v-navigation-drawer app>
+    </v-navigation-drawer>
+    <v-app-bar app>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
+    </v-footer>
   </v-app>
 </template>
-
-<script>
-import Header from '@/components/Header'
-
-export default {
-  components: {
-    Header
-  }
-}
-</script>
