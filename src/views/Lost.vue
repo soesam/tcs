886bd80ca17893de1ad342260a6a1958c7c058b4
soesam <67687007+soesam@users.noreply.github.com>
2020-07-13 14:57:00 +0100

    Update Lost.vue

--- src/views/Lost.vue
+++ src/views/Lost.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="lost">
-    404 not found
-  </div>
+  <v-container>
+    <v-layout>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <h2 class='display-3'>Woah, that page doesn't exist.</h2>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
