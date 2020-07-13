d3786df5de2cea597fcac22eb68d9f934b896bb1
soesam <67687007+soesam@users.noreply.github.com>
2020-07-13 14:34:54 +0100

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,22 @@
 <template>
-
+  <div></div>
 </template>
 
 <script>
 import { auth } from '@/app'
+
+export default {
+  data() {
+    email: '',
+    password: ''
+  },
+  
+  methods: {
+    submit() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(user => {
+        this.$router.replace('/home')
+      })
+    }
+  }
+}
 </script>
