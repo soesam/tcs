7a8e2b0d90b208b649c1f507bac601d9c95da8ef
root <root@ip-172-31-39-181.us-east-2.compute.internal>
2020-07-13 09:27:04 +0000

    added login guard boilerplate to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,19 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Start from '@/views/Start.vue';
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: "/start",
+    name: "start",
+    component: Start
   },
   {
-    path: "/home",
-    name: "Home",
+    path: "/",
+    name: "home",
     component: Home
   }
 ];
@@ -22,5 +23,10 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.name !== 'start' && !true /* TODO: firebase authentication check. */) next({name: 'start'})
+  else next()
+})
 
 export default router;
