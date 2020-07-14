009d51a227c144107cd92965d03c3ceaa2cdf500
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-14 13:39:13 +0000

    finishing off Guard.vue

--- src/components/Guard.vue
+++ src/components/Guard.vue
@@ -1,9 +1,46 @@
 <template>
-  <div>Guard component rendered with kind = {{ kind }}</div>
+  <v-container>
+    <v-layout>
+      <v-flex justify-center align-center>
+        <v-card>
+
+          <v-card-title>
+            <h2>{{ kind === 'signin' ? 'Sign in to TCS' : 'Create your account'}}</h2>
+          </v-card-title>
+
+          <v-card-text>
+            <v-form @submit.prevent="send">
+
+              <v-text-field
+                label='Your email' 
+                v-model='email'
+              >
+              </v-text-field>
+
+              <v-text-feild 
+                label='Password' 
+                v-model='password'
+              >
+              </v-text-field>
+
+              <v-btn flat>Create</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-  props: ["kind"]
+  props: ["kind"],
+  data: {
+    email: '',
+    password: ''
+  },
+  send() {
+    console.log('form submit fired')
+  }
 };
 </script>
