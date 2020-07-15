526bcf13dc2b0447852af9d94714a132a51cb9e5
Soe Sam <soesam65@gmail.com>
2020-07-15 16:58:03 +0100

    Updating

--- src/firebase.js
+++ src/firebase.js
@@ -15,6 +15,4 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const database = firebase.firestore();
 
-database.settings({timestampsInSnapshots: true });
-
 export default database;
