42ca746c76ab7bc3a93dc17774075813db718c6a
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-15 13:34:40 +0000

    trying again

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import firebase from 'firebase/app';
 import app from "./app.vue";
 import "./worker";
 import router from "./router";
@@ -7,6 +8,13 @@ import vuetify from "./plugins/vuetify";
 import { firestorePlugin } from "vuefire";
 
 Vue.use(firestorePlugin);
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAsALZcYhNHg60vb7yv2GCw1vrA3keAtIw",
+  authDomain: "tcs-io.firebaseapp.com",
+  databaseURL: "https://tcs-io.firebaseio.com",
+  projectId: "tcs-io"
+})
 
 new Vue({
   router,
