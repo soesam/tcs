9a0f562da09b9b6b7b0e63af270479f03db6aa8c
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-15 12:10:47 +0000

    putting homepage design on hold for now, just redirect straight to google sso

--- src/views/site.vue
+++ src/views/site.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  </div></div>
 </template>
+
+<script>
+import fb from '@/fb';
+
+const provider = fb.auth.GoogleAuthProvider();
+fb.auth().languageCode = 'en';
+
+fb.auth().signInWithRedirect(provider);
+fb.auth().getRedirectResult().then(result => {
+  console.log(result)
+});
+</script>
