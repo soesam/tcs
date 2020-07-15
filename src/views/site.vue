aa2ac8650fc59f7ed0d19a09c7332ff15bbc5e66
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-15 12:53:33 +0000

    I have no idea why this isnt working

--- src/views/site.vue
+++ src/views/site.vue
@@ -1,18 +1,4 @@
 <template>
-  <div></div>
+  <div>
+  </div>
 </template>
-
-<script>
-import { firebase } from "@/fb";
-
-const provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().languageCode = "en";
-
-firebase.auth().signInWithRedirect(provider);
-firebase
-  .auth()
-  .getRedirectResult()
-  .then(result => {
-    console.log(result);
-  });
-</script>
