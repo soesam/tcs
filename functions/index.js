2849dd7f68afba1196b1e38620089effad7d8405
Soe Sam <soesam65@gmail.com>
2020-07-15 12:39:12 +0100

    Adding firebase functions

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const env = functions.config();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+  var Airtable = require("airtable");
+
+  var base = new Airtable({ apiKey: "keyLVnvjV4bFHXOaD" }).base(
+    "appvLWxrF80mDK8Xq"
+  );
+
+  exports.addToAirtable = functions.firestore
+    .document('students/{Points}')
+    .onCreate((snap, context) => {
+      const data = snap.data();
+
+      return base('Students').create(data)
+    });
