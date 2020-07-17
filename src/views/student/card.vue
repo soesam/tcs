c132c451e369b6cbf9fafeeb90bcc7ea08fa6e54
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-17 12:00:19 +0000

    template card

---
+++ src/views/student/card.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-card-text>
+      <div>Revision</div>
+      <p class="display-1 text--primary">
+        Matrix multiplication
+      </p>
+      <p>Year 11 Maths • Mr Hart</p>
+      <div class="text--primary">
+        lorem ipsum dolor sit amet.
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn
+        text
+        color="teal darken-1"
+      >
+        start
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
