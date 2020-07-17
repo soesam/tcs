76313ef301d3fab559efbced07ac1a72074b7bda
soesam <soesam65@gmail.com>
2020-07-17 09:54:44 +0100

    Adding classes to master branch

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Classes from "../views.Classes.vue";
+
 
 Vue.use(VueRouter);
 
@@ -18,6 +20,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/classes",
+    name: "Classes",
+    component: Classes
   }
 ];
 
