98abe0c68aefccf6b3139998a2e01b5c2a1a9dd4
Cloud User <ec2-user@ip-172-31-27-14.us-east-2.compute.internal>
2020-07-20 07:57:56 +0000

    first step towards separating navigation from individual pages

---
+++ src/components/navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav>
+    <v-navigation-drawer app clipped v-model="drawer">
+      <v-list>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title color="amber">My homework</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Me</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="teal" elevation="3" dark clipped-left app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase">
+        <span style="font-weight: bold;">TCS</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text tile>
+        <span>sign out</span>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
